9d58c46453147a1b9f29c18157468940aa937f32
tonie zulu <92599780+tonie-zulu@users.noreply.github.com>
2022-07-28 09:54:31 +0200

    Add files via upload

---
+++ 100 Days Of Code/Day 1/hello_world.py
@@ -0,0 +1,17 @@
+#print function
+print('Hello World!')
+
+#variable and input function
+name = input('What is your name? ')
+
+print(f'Your name is {name.capitalize()}!')
+
+#Band_Name_Generator
+print('Hello! Welcome to the Band Name Generator!')
+
+name_of_city = input('What is the name of the city that you grew in? ')
+
+name_of_pet = input('What is the name of your pet? ')
+
+print(f'The band name for your pet is {name_of_city.capitalize()} - {name_of_pet.capitalize()}. Thank you for using the Band name generator!')
+
